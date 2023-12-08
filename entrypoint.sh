99019946b68651f2f98070eda7e89266afa99bd4
Lentin <codexss@users.noreply.github.com>
2023-12-08 23:30:56 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd /root
 apk update
 apk add --no-cache wget unzip
 wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip ./Xray-linux-64.zip
+unzip -o ./Xray-linux-64.zip
 rm ./Xray-linux-64.zip
 apk del wget unzip
 ./xray -c /etc/xray/config.json
