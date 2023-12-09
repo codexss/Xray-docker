ad2b200c0c7c68bbff566944296c6f613bc33240
Lentin <codexss@users.noreply.github.com>
2023-12-09 11:48:39 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /root
-wget -qO- --no-check-certificate https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip | unzip -o -
+wget -qO- --no-check-certificate https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip | unzip -q -
 chmod +x xray
 ./xray -c ./config.json
